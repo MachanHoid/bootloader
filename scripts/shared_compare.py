48de287edd71e98d5c284220c0b14b9d2ae6b266
adithyaa-rg <adithyaa2003@gmail.com>
2023-10-05 00:45:23 +0530

    New Linker Generation

--- scripts/shared_compare.py
+++ scripts/shared_compare.py
@@ -34,3 +34,36 @@ difference.write('\n'.join(bootloader_difference_driverlib))
 
 intersection.close()
 difference.close()
+
+
+shared_ro = open(f'build/helper_files_temp/shared_files/sharedlib_syms/local_readonly.txt')
+shared_ro_data = set()
+
+shared_ro_element = shared_ro.readline()
+while (shared_ro_element):
+    shared_ro_data.add(shared_ro_element.split()[0])
+    shared_ro_element = shared_ro.readline()
+
+unopt_bootloader_all = open(f'build/helper_files_temp/shared_files/unopt_bootloader_funcs.txt')
+unopt_bootloader_all_data = set()
+
+bootloader_element = unopt_bootloader_all.readline()
+while (bootloader_element):
+    # print(bootloader_element)
+    unopt_bootloader_all_data.add(bootloader_element.split()[0])
+    bootloader_element = unopt_bootloader_all.readline()
+
+intersection_data = unopt_bootloader_all_data.intersection(shared_ro_data)
+difference_data = unopt_bootloader_all_data.difference(shared_ro_data)
+
+shared_ro.close()
+unopt_bootloader_all.close()
+
+intersection = open(f'intersection_rodata.txt','w')
+difference = open(f'difference_rodata.txt','w')
+
+intersection.write('\n'.join(intersection_data))
+difference.write('\n'.join(difference_data))
+
+intersection.close()
+difference.close()
--- scripts/shared_make_linker.py
+++ scripts/shared_make_linker.py
@@ -1,6 +1,8 @@
 linker = open("linkers/shared_linker.ld",'r')
 function = open(f"build/helper_files_temp/shared_files/bootloader_intersection_sharedlib.txt",'r')
 notFunction = open(f"build/helper_files_temp/shared_files/bootloader_difference_sharedlib.txt",'r')
+roData_intersection = open(f"intersection_rodata.txt",'r')
+roData_difference = open(f"difference_rodata.txt",'r')
 newLinker = open("build/linkers_temp/shared_linker_new.ld",'w')
 
 new_linker = ''
@@ -12,8 +14,17 @@ noFunctions = [i.split()[0] for i in notFunction.readlines()]
 noFunctions = ['*(.text.' + i + ')' for i in noFunctions]
 noFunctions = '\n\t\t'.join(noFunctions)
 
+roData_intersections = [i.split()[0] for i in roData_intersection.readlines()]
+roData_intersections = ['*(.rodata.' + i + ')' for i in roData_intersections]
+roData_intersections = '\n\t\t'.join(roData_intersections)
+
+roData_differences = [i.split()[0] for i in roData_difference.readlines()]
+roData_differences = ['*(.rodata.' + i + ')' for i in roData_differences]
+roData_differences = '\n\t\t'.join(roData_differences)
+
 checkerDiscard = 0
 checkerFunction = 0
+checkerRO = 0
 linker_lines = linker.readlines()
 
 for i in linker_lines:
@@ -27,8 +38,14 @@ for i in linker_lines:
         new_linker += '\n/DISCARD/ :{'
         new_linker += '\n\t\t'
         new_linker += noFunctions
+        new_linker += roData_differences
         new_linker += '\n\t}\n'
         checkerDiscard = 1
+    elif i.strip() == '*(.rodata*)' and checkerRO == 0:
+        new_linker += '\n\t\t'
+        new_linker += roData_intersections
+        new_linker += '\n'
+        checkerRO = 1
     else:
         new_linker += i
 newLinker.write(new_linker)
